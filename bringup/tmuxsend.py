efcafe25db014ad7a493fccefed74e9239a5c030
Luca Iocchi <iocchi@diag.uniroma1.it>
2018-11-01 10:19:02 +0100

    bringup update

---
+++ bringup/tmuxsend.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+class TmuxSend:
+
+    # listwindows = [ 'windowname', ... ]
+    def __init__(self, sessionname, listwindows):
+        self.nwindows = len(listwindows)
+        self.sessionname = sessionname
+        os.system('tmux -2 new-session -d -s %s' %self.sessionname) # tmux session
+        os.system('tmux select-window -t %s:0' %self.sessionname)
+        os.system('tmux rename-window \'quit\'')                  # window 0 - quit
+        for i in range(0,self.nwindows):
+            os.system('tmux new-window -t %s:%d -n \'%s\'' %(self.sessionname, i+1, listwindows[i]))    
+
+    def roslaunch(self, wid, mdir, mlaunch, mparams=''):
+        os.system('tmux select-window -t %s:%d' %(self.sessionname,wid))
+        os.system('tmux send-keys "cd $MARRTINO_APPS_HOME/%s" C-m' %(mdir))
+        os.system('tmux send-keys "roslaunch %s.launch %s" C-m' %(mlaunch, mparams))
+
+    def python(self, wid, mdir, mpy, mparams=''):
+        os.system('tmux select-window -t %s:%d' %(self.sessionname,wid))
+        os.system('tmux send-keys "cd $MARRTINO_APPS_HOME/%s" C-m' %(mdir))
+        os.system('tmux send-keys "python %s %s" C-m' %(mpy, mparams))
+
+    def cmd(self, wid, cmd):
+        os.system('tmux select-window -t %s:%d' %(self.sessionname,wid))
+        os.system('tmux send-keys "%s" C-m' %(cmd))
+
+    def kill(self, rosnode):
+        wid = 0
+        os.system('tmux select-window -t %s:%d' %(self.sessionname,wid))
+        os.system('tmux send-keys "rosnode kill %s" C-m' %(rosnode))
+
+    def killall(self, wid):
+        self.Cc(wid)
+        time.sleep(3)
+        self.Ck(wid)
+
+    def Cc(self, wid):
+        os.system('tmux select-window -t bringup:%d' %(wid))
+        os.system('tmux send-keys C-c')
+
+    def Ck(self, wid):
+        os.system('tmux select-window -t bringup:%d' %(wid))
+        os.system('tmux send-keys C-\\')
+
+    def quitall(self):
+        self.kill('-a')
+        time.sleep(3)
+        for i in range(0,self.nwindows):
+            self.Cc(i+1)  # C-c on all the windows
+            time.sleep(1)
+        for i in range(0,self.nwindows):
+            self.Ck(i+1)  # C-\ on all the windows
+            time.sleep(1)
+
+
--- bringup/wsbringup.py
+++ bringup/wsbringup.py
@@ -16,58 +16,18 @@ sys.path.append('../scripts')
 import check
 from check import *
 
+from tmuxsend import TmuxSend
+
+
 # Global variables
 
 websocket_server = None     # websocket handler
 run = True                  # main_loop run flag
+server_name = 'Bringup'     # server name
 server_port = 9500          # web server port
 status = "Idle"             # robot status sent to websocket
-nwindows = 0
 
-def tmux_init():
-    global nwindows
-    os.system('tmux -2 new-session -d -s bringup') # tmux session
-    os.system('tmux select-window -t bringup:0')
-    os.system('tmux rename-window \'quit\'')                  # window 0 - quit
-    os.system('tmux new-window -t bringup:1 -n \'robot\'')    # window 1 - robot
-    os.system('tmux new-window -t bringup:2 -n \'laser\'')    # window 2 - laser
-    os.system('tmux new-window -t bringup:3 -n \'camera\'')   # window 3 - camera
-    os.system('tmux new-window -t bringup:4 -n \'joystick\'') # window 4 - joystick
-    os.system('tmux new-window -t bringup:5 -n \'audio\'')    # window 5 - audio
-    os.system('tmux new-window -t bringup:6 -n \'wsrobot\'')  # window 6 - WS robot
-    nwindows = 6
 
-def tmux_launch(wid, mdir, mlaunch, mparams=''):
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys "cd $MARRTINO_APPS_HOME/%s" C-m' %(mdir))
-    os.system('tmux send-keys "roslaunch %s.launch %s" C-m' %(mlaunch, mparams))
-
-def tmux_python(wid, mdir, mpy, mparams=''):
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys "cd $MARRTINO_APPS_HOME/%s" C-m' %(mdir))
-    os.system('tmux send-keys "python %s %s" C-m' %(mpy, mparams))
-
-def tmux_cmd(wid, cmd):
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys "%s" C-m' %(cmd))
-
-def tmux_kill(rosnode):
-    wid = 0
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys "rosnode kill %s" C-m' %(rosnode))
-
-def tmux_killall(wid):
-    tmux_Cc(wid)
-    time.sleep(wid)
-    tmux_Ck(wid)
-
-def tmux_Cc(wid):
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys C-c')
-
-def tmux_Ck(wid):
-    os.system('tmux select-window -t bringup:%d' %(wid))
-    os.system('tmux send-keys C-\\')
 
 
 # Websocket server handler
@@ -116,22 +76,8 @@ class MyWebSocketServer(tornado.websocket.WebSocketHandler):
         global websocket_server, run
         websocket_server = self
         print('New connection')
+        self.tmux = TmuxSend('bringup',['robot','laser','camera','joystick','audio','wsrobot'])
         self.checkStatus()
-
-    def quitall(self):
-        tmux_kill('-a')
-        time.sleep(3)
-        i=1
-        while (i<=nwindows):
-            tmux_Cc(i)  # C-c on all the windows
-            time.sleep(1)
-            i += 1
-        i=1
-        while (i<=nwindows):
-            tmux_Ck(i)  # C-c on all the windows
-            time.sleep(1)
-            i += 1
-
 
     def on_message(self, message):
         global code, status
@@ -144,153 +90,153 @@ class MyWebSocketServer(tornado.websocket.WebSocketHandler):
             self.checkStatus()
 
         elif (message=='ros_quit'):
-            self.quitall()
+            self.tmux.quitall()
             self.checkStatus()
 
         elif (message=='robot_start'):
-            tmux_launch(1,'robot','robot')
+            self.tmux.roslaunch(1,'robot','robot')
             time.sleep(3)
             self.checkStatus()
         elif (message=='robot_kill'):
-            tmux_kill('orazio')
-            tmux_kill('state_pub_robot')
+            self.tmux.kill('orazio')
+            self.tmux.kill('state_pub_robot')
             time.sleep(3)
-            tmux_killall(1)
+            self.tmux.killall(1)
             self.checkStatus()
 
         elif (message=='simrobot_start'):
-            tmux_launch(1,'stage','simrobot')
+            self.tmux.roslaunch(1,'stage','simrobot')
             time.sleep(3)
             self.checkStatus()
         elif (message=='simrobot_kill'):
-            tmux_kill('stageros')
+            self.tmux.kill('stageros')
             time.sleep(3)
-            tmux_killall(1)
+            self.tmux.killall(1)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='usbcam_start'):
-            tmux_launch(3,'camera','usbcam')
+            self.tmux.roslaunch(3,'camera','usbcam')
             time.sleep(3)
             self.checkStatus()
         elif (message=='usbcam_kill'):
-            tmux_kill('usb_cam')
-            tmux_kill('state_pub_usbcam')
+            self.tmux.kill('usb_cam')
+            self.tmux.kill('state_pub_usbcam')
             time.sleep(3)
-            tmux_killall(3)
+            self.tmux.killall(3)
             time.sleep(3)
             self.checkStatus()
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='astra_start'):
-            tmux_launch(3,'camera','astra')
+            self.tmux.roslaunch(3,'camera','astra')
             time.sleep(3)
             self.checkStatus()
         elif (message=='astra_kill'):
-            tmux_kill('astra')
-            tmux_kill('state_pub_astra')
+            self.tmux.kill('astra')
+            self.tmux.kill('state_pub_astra')
             time.sleep(3)
-            tmux_killall(3)
+            self.tmux.killall(3)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='xtion_start'):
-            tmux_launch(3,'camera','xtion')
+            self.tmux.roslaunch(3,'camera','xtion')
             time.sleep(3)
             self.checkStatus()
         elif (message=='xtion_kill'):
-            tmux_kill('xtion')
-            tmux_kill('state_pub_xtion')
+            self.tmux.kill('xtion')
+            self.tmux.kill('state_pub_xtion')
             time.sleep(3)
-            tmux_killall(3)
+            self.tmux.killall(3)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='hokuyo_start'):
-            tmux_launch(2,'laser','hokuyo')
+            self.tmux.roslaunch(2,'laser','hokuyo')
             time.sleep(3)
             self.checkStatus()
         elif (message=='hokuyo_kill'):
-            tmux_kill('hokuyo')
-            tmux_kill('state_pub_laser')
+            self.tmux.kill('hokuyo')
+            self.tmux.kill('state_pub_laser')
             time.sleep(3)
-            tmux_killall(2)
+            self.tmux.killall(2)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='rplidar_start'):
-            tmux_launch(2,'laser','rplidar')
+            self.tmux.roslaunch(2,'laser','rplidar')
             time.sleep(3)
             self.checkStatus()
         elif (message=='rplidar_kill'):
-            tmux_kill('rplidar')
-            tmux_kill('state_pub_laser')
+            self.tmux.kill('rplidar')
+            self.tmux.kill('state_pub_laser')
             time.sleep(3)
-            tmux_killall(2)
+            self.tmux.killall(2)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='astralaser_start'):
-            tmux_launch(2,'laser','astra_laser')
+            self.tmux.roslaunch(2,'laser','astra_laser')
             time.sleep(3)
             self.checkStatus()
         elif (message=='astralaser_kill'):
-            tmux_kill('astra')
-            tmux_kill('depth2laser')
-            tmux_kill('state_pub_astra_laser')
+            self.tmux.kill('astra')
+            self.tmux.kill('depth2laser')
+            self.tmux.kill('state_pub_astra_laser')
             time.sleep(3)
-            tmux_killall(2)
+            self.tmux.killall(2)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='xtionlaser_start'):
-            tmux_launch(2,'laser','xtion_laser')
+            self.tmux.roslaunch(2,'laser','xtion_laser')
             time.sleep(3)
             self.checkStatus()
         elif (message=='xtionlaser_kill'):
-            tmux_kill('xtion')
-            tmux_kill('depth2laser')
-            tmux_kill('state_pub_xtion_laser')
+            self.tmux.kill('xtion')
+            self.tmux.kill('depth2laser')
+            self.tmux.kill('state_pub_xtion_laser')
             time.sleep(3)
-            tmux_killall(2)
+            self.tmux.killall(2)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='joystick_start'):
-            tmux_launch(4,'teleop','teleop')
+            self.tmux.roslaunch(4,'teleop','teleop')
             time.sleep(3)
             self.checkStatus()
         elif (message=='joystick_kill'):
-            tmux_kill('joystick')
-            tmux_kill('joy')
+            self.tmux.kill('joystick')
+            self.tmux.kill('joy')
             time.sleep(3)
-            tmux_killall(4)
+            self.tmux.killall(4)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='audio_start'):
-            tmux_python(5,'audio','audio_server.py')
+            self.tmux.python(5,'audio','audio_server.py')
             time.sleep(3)
             self.checkStatus()
         elif (message=='audio_kill'):
-            tmux_killall(5)
+            self.tmux.killall(5)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='wsrobot_start'):
-            tmux_python(6,'blockly','websocket_robot.py')
+            self.tmux.python(6,'blockly','websocket_robot.py')
             time.sleep(3)
             self.checkStatus()
         elif (message=='wsrobot_kill'):
-            tmux_killall(6)
+            self.tmux.killall(6)
             time.sleep(3)
             self.checkStatus()
 
         elif (message=='shutdown'):
-            self.quitall()
+            self.tmux.quitall()
             self.checkStatus()
-            tmux_cmd(1,'sudo shutdown -h now')
+            self.tmux.cmd(1,'sudo shutdown -h now')
 
         else:
             print('Code received:\n%s' %message)
@@ -327,7 +273,7 @@ def main_loop(data):
                 websocket_server.write_message("STATUS "+status)
                 #print(status)
             except tornado.websocket.WebSocketClosedError:
-                #print('-- WebSocketClosedError --')
+                # print('-- WebSocketClosedError --')
                 websocket_server = None
     print("Main loop quit.")
 
@@ -345,8 +291,6 @@ def run_code(code):
 
 if __name__ == "__main__":
 
-    tmux_init()
-
     # Run main thread
     t = Thread(target=main_loop, args=(None,))
     t.start()
@@ -356,7 +300,7 @@ if __name__ == "__main__":
         (r'/websocketserver', MyWebSocketServer),])  
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(server_port)
-    print("Bringup Websocket server listening on port %d" %(server_port))
+    print("%s Websocket server listening on port %d" %(server_name,server_port))
     try:
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
@@ -364,7 +308,7 @@ if __name__ == "__main__":
 
     if (not websocket_server is None):
         websocket_server.close()
-    print("Bringup Websocket server quit.")
+    print("%s Websocket server quit." %server_name)
     run = False    
     print("Waiting for main loop to quit...")
 
