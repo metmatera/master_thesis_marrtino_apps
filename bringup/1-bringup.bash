558a2d20e84b06466458ea161c47475f95145736
Luca Iocchi <iocchi@diag.uniroma1.it>
2018-11-10 09:42:15 +0100

    bringup bash

--- bringup/1-bringup.bash
+++ bringup/1-bringup.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 date
+#source /home/ubuntu/.bashrc
 source /home/ubuntu/ros/catkin_ws/devel/setup.bash
-source /home/ubuntu/.bashrc
 export MARRTINO_APPS_HOME=/home/ubuntu/src/marrtino_apps
+export DISPLAY=:0
 cd /home/ubuntu/src/marrtino_apps/bringup
 python wsbringup.py &
 cd /home/ubuntu/src/marrtino_apps/config
