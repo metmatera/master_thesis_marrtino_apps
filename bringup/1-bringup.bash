22e36443f1762d98c30c81aa58272cb77db125bc
Luca Iocchi <iocchi@diag.uniroma1.it>
2019-08-02 10:32:19 +0200

    default ROBOT_TYPE marrtino in bringup init script

--- bringup/1-bringup.bash
+++ bringup/1-bringup.bash
@@ -5,6 +5,7 @@ source /home/ubuntu/ros/catkin_ws/devel/setup.bash
 export MARRTINO_APPS_HOME=/home/ubuntu/src/marrtino_apps
 export MODIM_HOME=/home/ubuntu/src/modim
 export DISPLAY=:0
+export ROBOT_TYPE=marrtino
 cd /home/ubuntu/src/marrtino_apps/bringup
 python wsbringup.py &
 cd /home/ubuntu/src/marrtino_apps/config
