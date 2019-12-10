e236e894d680bff55ddfa14f9b5a53060ca772a0
Luca Iocchi <iocchi@diag.uniroma1.it>
2019-12-10 18:57:34 +0100

    upload firmware script

---
+++ config/uploadfirmware.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $HOME/src/srrg/srrg2_orazio/srrg2_orazio/firmware_build/atmega2560
+make orazio.elf
+avr-objcopy -O ihex -R .eeprom orazio.elf orazio.hex
+avrdude -p m2560 -P /dev/orazio   -c  -b 115200    -D -q -V -C /usr/share/arduino/hardware/tools/avr/../avrdude.conf -c wiring -U flash:w:orazio.hex:i
+
+
