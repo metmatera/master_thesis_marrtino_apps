41a1ea53adb7981f96bcdadcd8f69cbd1e2fad51
Luca Iocchi <iocchi@diag.uniroma1.it>
2019-12-11 14:28:41 +0100

    uploadfirmware

--- config/uploadfirmware.bash
+++ config/uploadfirmware.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $HOME/src/srrg/srrg2_orazio/srrg2_orazio/firmware_build/atmega2560
+make clean
 make orazio.elf
 avr-objcopy -O ihex -R .eeprom orazio.elf orazio.hex
 avrdude -p m2560 -P /dev/orazio   -c  -b 115200    -D -q -V -C /usr/share/arduino/hardware/tools/avr/../avrdude.conf -c wiring -U flash:w:orazio.hex:i
