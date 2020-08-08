d2fd87016ca4f21ae723d2665c42404ca368f518
Luca Iocchi <iocchi@diag.uniroma1.it>
2020-08-08 15:54:15 +0200

    bringup -docker option

--- bringup/1-bringup.bash
+++ bringup/1-bringup.bash
@@ -1,14 +1,30 @@
 #!/bin/bash
 date
-#source /home/ubuntu/.bashrc
-source /home/ubuntu/ros/catkin_ws/devel/setup.bash
-export MARRTINO_APPS_HOME=/home/ubuntu/src/marrtino_apps
-export MODIM_HOME=/home/ubuntu/src/modim
+
+if [ ! "$1" == "-docker" ]; then
+  echo "docker runnning..."
+  sudo service nginx start
+fi
+
+#source $HOME/.bashrc
+source $HOME/ros/catkin_ws/devel/setup.bash
+export MARRTINO_APPS_HOME=$HOME/src/marrtino_apps
+export MODIM_HOME=$HOME/src/modim
 export DISPLAY=:0
 export ROBOT_TYPE=marrtino
-cd /home/ubuntu/src/marrtino_apps/bringup
+cd $HOME/src/marrtino_apps/bringup
 python wsbringup.py &
-cd /home/ubuntu/src/marrtino_apps/config
+cd $HOME/src/marrtino_apps/config
 python wsconfig.py &
 roscore &
+
+if [ ! "$1" == "-docker" ]; then
+  echo "IP addresses: `hostname -I`"
+  echo "docker exec -it <container name>  /usr/bin/tmux   for shell access"
+  echo "docker exec -it <container name> touch /tmp/quitrequest   to quit the docker container"
+  while [ ! -f "/tmp/quitrequest" ]; do
+    sleep 5
+  done
+  rm /tmp/quitrequest
+fi
 
