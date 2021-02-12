f8c924951f10c10e35e3b34674839ac31404dacc
iocchi <iocchi@diag.uniroma1.it>
2021-02-12 15:03:10 +0000

    docker 2018; docker this_state_publisher patch; run_orazio_web

--- config/run_orazio_web.bash
+++ config/run_orazio_web.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rosrun srrg2_orazio_core orazio -serial-device /dev/orazio -resource-path $HOME/src/srrg/srrg2_orazio_core/html
+rosrun srrg2_orazio orazio -serial-device /dev/orazio -resource-path $HOME/src/srrg/srrg2_orazio/srrg2_orazio/html
 
 
