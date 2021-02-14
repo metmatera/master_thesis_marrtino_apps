c408594f51943c9b6608b12b588d4eff3ac6aebc
Luca Iocchi <iocchi@diag.uniroma1.it>
2021-02-14 18:31:12 +0100

    stop docker + prune

--- docker/stop_docker.bash
+++ docker/stop_docker.bash
@@ -39,3 +39,5 @@ tmux new-window -t $SESSION:1 -n 'compose down'
 
 tmux send-keys -t $SESSION:1 "cd \$MARRTINO_APPS_HOME/docker && docker-compose --profile $DOCKER_PROFILE down" C-m
 
+tmux send-keys -t $SESSION:1 "docker container prune -f" C-m
+
