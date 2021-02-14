0893a24a464c2561f2997edd7a2a5d34aac675ff
Luca Iocchi <iocchi@diag.uniroma1.it>
2021-02-14 18:26:05 +0100

    system update

--- docker/start_docker.bash
+++ docker/start_docker.bash
@@ -15,11 +15,27 @@ if [ "$MBTYPE" = "" ]; then
   echo "Using default value: $MBTYPE"
 fi
 
+if [ "$MACHTYPE" == "aarch64" ] || [ "$MACHTYPE" == "armv7l" ]; then
+  ARCH="arm64"
+  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
+    DOCKER_PROFILE="robot2018arm64"
+  else
+    DOCKER_PROFILE="robotarm64"
+  fi
+else
+  ARCH=""
+  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
+    DOCKER_PROFILE="robot2018"
+  else
+    DOCKER_PROFILE="robot"
+  fi
+fi
+
 SESSION=compose
 
 tmux -2 new-session -d -s $SESSION
 
 tmux rename-window -t $SESSION:0 'compose up'
 
-tmux send-keys -t $SESSION:0 "cd \$MARRTINO_APPS_HOME/docker && docker-compose --profile $SYSTEM_PROFILE up" C-m
+tmux send-keys -t $SESSION:0 "cd \$MARRTINO_APPS_HOME/docker && docker-compose --profile $DOCKER_PROFILE up" C-m
 
--- docker/stop_docker.bash
+++ docker/stop_docker.bash
@@ -15,11 +15,27 @@ if [ "$MBTYPE" = "" ]; then
   echo "Using default value: $MBTYPE"
 fi
 
+if [ "$MACHTYPE" == "aarch64" ] || [ "$MACHTYPE" == "armv7l" ]; then
+  ARCH="arm64"
+  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
+    DOCKER_PROFILE="robot2018arm64"
+  else
+    DOCKER_PROFILE="robotarm64"
+  fi
+else
+  ARCH=""
+  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
+    DOCKER_PROFILE="robot2018"
+  else
+    DOCKER_PROFILE="robot"
+  fi
+fi
+
 SESSION=compose
 
 tmux -2 new-session -d -s $SESSION
 
 tmux new-window -t $SESSION:1 -n 'compose down'
 
-tmux send-keys -t $SESSION:1 "cd \$MARRTINO_APPS_HOME/docker && docker-compose --profile $SYSTEM_PROFILE down" C-m
+tmux send-keys -t $SESSION:1 "cd \$MARRTINO_APPS_HOME/docker && docker-compose --profile $DOCKER_PROFILE down" C-m
 
