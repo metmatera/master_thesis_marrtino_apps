36e3f4af512dba78123cd78f1fd46172ab235223
Luca Iocchi <iocchi@diag.uniroma1.it>
2021-02-16 21:14:50 +0100

    system update

--- docker/system_update.bash
+++ docker/system_update.bash
@@ -2,65 +2,7 @@
 
 source stop_docker.bash 
 
-MACHTYPE=`uname -m`
-MBTYPE=`cat $HOME/.marrtino_motorboard`
-SYSTEM_PROFILE=`cat $HOME/.system_profile`
-
-if [ "$SYSTEM_PROFILE" = "" ]; then
-  echo "System profile not set. File $HOME/.system_profile is empty." 
-  exit 1
-fi
-
-if [ "$MBTYPE" = "" ]; then
-  echo "Motor board not set. File $HOME/.marrtino_motorboard is empty."
-  MBTYPE="MARRtinoMB2019"
-  echo "Using default value: $MBTYPE"
-fi
-
-if [ "$MACHTYPE" == "aarch64" ] || [ "$MACHTYPE" == "armv7l" ]; then
-  ARCH="arm64"
-  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
-    ORAZIO_TAG="2018-arm64"
-    DOCKER_PROFILE="robot2018arm64"
-  else
-    ORAZIO_TAG="arm64"
-    DOCKER_PROFILE="robotarm64"
-  fi
-else
-  ARCH=""
-  if [ "$MBTYPE" = "ArduinoMotorShield" ]; then
-    ORAZIO_TAG="2018"
-    DOCKER_PROFILE="robot2018"
-  else
-    ORAZIO_TAG=""
-    DOCKER_PROFILE="robot"
-  fi
-fi
-
-
-echo "System update"
-echo "- system arch: $MACHTYPE"
-echo "- system profile: $SYSTEM_PROFILE"
-echo "- motor board: $MBTYPE"
-echo "- docker profile: $DOCKER_PROFILE"
-echo "- orazio tag: $ORAZIO_TAG"
-
-
-docker pull iocchi/rchomeedu-1804-melodic:${ARCH}
-
-cd $HOME/src/marrtino_apps && git pull
-cd $HOME/src/rc-home-edu-learn-ros && git pull
-
-if [ "$SYSTEM_PROFILE" = "simulator" ]; then
-  docker pull iocchi/stage_environments:${ARCH}
-  cd $HOME/src/stage_environments && git pull
-fi
-
-if [ "$SYSTEM_PROFILE" = "robot" ]; then
-  docker pull iocchi/orazio:${ORAZIO_TAG}
-fi
-
-cd $MARRTINO_APPS_HOME/docker && docker-compose --profile $DOCKER_PROFILE build
+cd $MARRTINO_APPS_HOME/docker && python3 dockerconfig.py && docker-compose build
 
 docker container prune -f
 docker image prune -f
