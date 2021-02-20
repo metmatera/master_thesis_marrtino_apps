606cd31f0a597918fbbe469944e94df95ee467ce
Luca Iocchi <iocchi@diag.uniroma1.it>
2021-02-20 14:29:08 +0100

    docker navigation fix

--- docker/docker_build_all.bash
+++ docker/docker_build_all.bash
@@ -2,18 +2,18 @@
 
 MACHTYPE=`uname -m`
 
-docker build -t marrtino:base -f Dockerfile.base . && \
 if [ "$MACHTYPE" = "x86_64" ]; then
-  docker build -t iocchi/orazio -f Dockerfile.orazio . && \
+  docker build -t orazio -f Dockerfile.orazio . && \
+  docker tag orazio iocchi/orazio && \
   docker build -t iocchi/orazio:2018 -f Dockerfile.orazio2018 .
 else
-  docker build -t iocchi/orazio:arm64 -f Dockerfile.orazio . && \
+  docker build -t orazio -f Dockerfile.orazio . && \
+  docker tag orazio iocchi/orazio:arm64 && \
   docker build -t iocchi/orazio:2018-arm64 -f Dockerfile.orazio2018 .
 fi && \
+docker build -t marrtino:base -f Dockerfile.base . && \
 docker build -t marrtino:teleop -f Dockerfile.teleop . && \
 docker build -t marrtino:navigation -f Dockerfile.navigation . && \
 docker build -t marrtino:vision -f Dockerfile.vision . && \
 docker build -t marrtino:speech -f Dockerfile.speech . 
-
-
 
