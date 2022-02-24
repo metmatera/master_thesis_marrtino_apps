809cdf730c95b483b8be728bd5b9ed9e6fce33b5
Luca Iocchi <iocchi@diag.uniroma1.it>
2022-02-24 18:44:55 +0100

    system watchdog logincompleted

--- docker/system_watchdog.bash
+++ docker/system_watchdog.bash
@@ -2,7 +2,7 @@
 
 date
 
-rm -f ~/log/shutdownrequest ~/log/rebootrequest ~/log/systemupdate ~/log/dockerrestart
+rm -f ~/log/shutdownrequest ~/log/rebootrequest ~/log/systemupdate ~/log/dockerrestart ~/log/logincompleted
 
 echo "Waiting for login ..."
 
