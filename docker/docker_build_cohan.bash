05a706cc40babdf7f09d4924b82e50763ff65d40
iocchi <iocchi@diag.uniroma1.it>
2022-11-21 16:37:25 +0100

    docker_build_cohan

--- docker/docker_build_cohan.bash
+++ docker/docker_build_cohan.bash
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-DOCKERFILE=Dockerfile.navigation-cohan
-if [ "$1" != "" ]; then
-  DOCKERFILE=$1
-fi
-
-echo "Dockerfile: $DOCKERFILE"
-
-MACHTYPE=`uname -m`
-
 UPAR="--build-arg UID=`id -u` --build-arg GID=`id -g`"
 
-docker build $UPAR -t marrtino:navigation-cohan -f $DOCKERFILE .
+docker build $UPAR -t marrtino:system -f Dockerfile.system . && \
+docker build -t marrtino:base -f Dockerfile.base . && \
+docker build -t marrtino:navigation-cohan -f Dockerfile.navigation-cohan .
 
 
 
