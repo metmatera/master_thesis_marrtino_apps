35f47f209b2086e6b4379078f3a134ba0a4caf57
Matteo Matera <matera.1795339@studenti.uniroma1.it>
2023-02-16 18:25:59 +0100

    update startloc script

--- navigation/startloc.py
+++ navigation/startloc.py
@@ -6,6 +6,7 @@ import sys, os, time, math
 import rospy
 import rosnode
 import tf
+import argparse
 
 from nav_msgs.msg import Odometry
 
@@ -41,7 +42,7 @@ def groundtruth_cb(data):
 
 ### main ###
 
-def main(argv):
+def main(args):
     global gt_robot_pose
 
     rospy.init_node('autostart_localizer')
@@ -54,6 +55,7 @@ def main(argv):
     msrun = noderunning('/map_server')
     print('map_server running: %r' %(msrun))
 
+    '''
     # get map name
     mapname = 'map' # default map name
     if len(argv)>1:
@@ -63,12 +65,18 @@ def main(argv):
             mapname = rospy.get_param('/mapname')
         except:
             pass
-    print('map name: %s' %mapname)
+    '''
+
+    print('map name: %s' %args.mapname)
 
     if not msrun:
         #   check if mapname file exists - if not ERROR
-        print("TODO: check if map file %s exists" %mapname)
+        print("TODO: check if map file %s exists" %args.mapname)
 
+    mapdir = args.mapdir
+    if mapdir == None:
+        mapdir = os.getenv("MARRTINO_APPS_HOME")+"/mapping/maps"
+        # mapdir = os.getenv("HOME")+"/playground/maps"
 
 
     #get robot current pose from topic /base_pose_ground_truth
@@ -79,16 +87,23 @@ def main(argv):
 
     gt_sub.unregister()
 
-    if gt_robot_pose is None and len(argv)>4:
-        gt_robot_pose = [ float(argv[2]), float(argv[3]), float(argv[4])/180.0*math.pi ]
+    if gt_robot_pose is None and args.initpose != None:
+        pp = args.initpose.split()
+        gt_robot_pose = [0,0,0]
+        gt_robot_pose[0]=float(pp[0])
+        gt_robot_pose[1]=float(pp[1])
+        gt_robot_pose[2]=float(pp[2])/180.0*math.pi # rad
 
     print("Current robot pose: %r" %(gt_robot_pose))
 
+    # start mapserver
+    if args.mapserveronly:
+        rstr = "rosrun map_server map_server %s/%s.yaml" %(mapdir,args.mapname)
     #start localizer
-    if (msrun and stagerun):
+    elif (msrun and stagerun):
         rstr = "roslaunch amcl.launch use_mapserver:=false "
     else:
-        rstr = "roslaunch amcl.launch map_name:=%s use_mapserver:=true " %mapname
+        rstr = "roslaunch amcl.launch mapsdir:=%s map_name:=%s use_mapserver:=true " %(mapdir,args.mapname)
 
     if gt_robot_pose is not None:
         rstr += " initial_pose_x:=%.1f initial_pose_y:=%.1f  initial_pose_a:=%.3f" \
@@ -98,8 +113,18 @@ def main(argv):
     os.system(rstr)
 
 
-# Use startloc.py [<mapname>] [<x> <y> <th_deg>]
+# Use startloc.py <mapname> [-mapdir <DIR>] [-initpose <X Y TH_DEG>]
 
 if __name__=='__main__':
-    main(sys.argv)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mapname", type=str, default="map",  nargs='?', help="Map name")                  
+    parser.add_argument("-mapdir", type=str, default=None,   help="Map folder")
+    parser.add_argument("-initpose", type=str, default=None,
+        help="Init pose in the form 'X Y Th_deg' (default: None)")
+    parser.add_argument('--mapserveronly', default = False, action ='store_true', 
+        help='Start only map_server node')
+
+    args = parser.parse_args()
+
+    main(args)
