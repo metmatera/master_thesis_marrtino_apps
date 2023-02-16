a426dd933b630f7c3edd43cd4b2891a361bfa032
Matteo Matera <matera.1795339@studenti.uniroma1.it>
2023-02-16 20:08:48 +0100

    fixed agents bridge script

--- stage/agents_bridge.py
+++ stage/agents_bridge.py
@@ -1,6 +1,6 @@
 import rospy, time, sys
 
-from cohan_msgs.msg import TrackedAgents, TrackedAgent, TrackedSegmentType, TrackedSegment
+from cohan_msgs.msg import TrackedAgents, TrackedAgent, TrackedSegmentType, TrackedSegment, AgentType
 from people_msgs.msg import PositionMeasurement, PositionMeasurementArray
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point
@@ -12,24 +12,73 @@ class StageAgents(object):
 	def __init__(self):
 		self.tracked_agents_pub = []
 		self.Segment_Type = TrackedSegmentType.TORSO
-		self.n_agents = 0
-		self.tmp_n_agents = self.n_agents
+		self.Agent_State = TrackedAgent.MOVING
+		self.Agent_Type = AgentType.HUMAN
 	
 	def AgentsPub(self):
 		rospy.init_node('Stage_Agents', anonymous=True)
 		ptm_sub = rospy.Subscriber("/people_tracker_measurements", PositionMeasurementArray, self.AgentsCB)
+		'''
+		* std_msgs/Header
+		
+		uint32 	seq
+		time 	stamp
+		string 	frame_id
+		
+		* people_msgs/PositionMeasurementArray
+		
+		std_msgs/Header 			header
+		people_msgs/PositionMeasurement[] 	people
+		float32[] 				cooccurence
+		
+		* people_msgs/PositionMeasurement
+		
+		std_msgs/Header 	header
+		string 			name
+		geometry_msgs/Point 	pos
+		float64 		reliability
+		float64[9] 		covariance
+		byte 			initialization
+		'''
+		
 		self.tracked_agents_pub = rospy.Publisher("/tracked_agents", TrackedAgents, queue_size=1)
+		'''
+		* cohan_msgs/TrackedAgents
+		
+		std_msgs/Header 		header
+		cohan_msgs/TrackedAgent[] 	agents
+		
+		* cohan_msgs/TrackedAgent
+		
+		uint64 				track_id
+		int8 				state
+		int8 				type
+		string 				name
+		cohan_msgs/TrackedSegment[] 	segments
+		
+		* cohan_msgs/TrackedSegment
+		
+		int8 					type
+		geometry_msgs/PoseWithCovariance 	pose
+		geometry_msgs/TwistWithCovariance 	twist
+		geometry_msgs/AccelWithCovariance 	accel
+		'''
+		
 		rospy.spin()
 	
 	def AgentsCB(self, msg):
 		tracked_agents = TrackedAgents()
 		for agent in msg.people:
+		
+			tracked_agent = TrackedAgent()
+			tracked_agent.state = self.Agent_State
+			tracked_agent.type = self.Agent_Type
+			tracked_agent.name = agent.name
 			agent_segment = TrackedSegment()
 			agent_segment.type = self.Segment_Type
-			agent_segment.pose.pose = agent.pos
-			tracked_agent = TrackedAgent()
-			tracked_agent.track_id = agent.object_id
+			agent_segment.pose.pose.position = agent.pos
 			tracked_agent.segments.append(agent_segment)
+			
 			tracked_agents.agents.append(tracked_agent)
 			
 		if (tracked_agents.agents):
@@ -37,11 +86,6 @@ class StageAgents(object):
 			tracked_agents.header.frame_id = 'map'
 			self.tracked_agents_pub.publish(tracked_agents)
 			
-		# Debug print
-		#self.n_agents = len(tracked_agents.agents)
-		#if (self.n_agents != self.tmp_n_agents):
-		#	print "Number of tracked agents: " + str(self.n_agents)
-		#	self.tmp_n_agents = self.n_agents
 
 if __name__ == '__main__':
 	agents = StageAgents()
