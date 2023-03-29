e202c93b00351aad474ec042c6c6e1fe3a4168fc
metmatera <matera.1795339@studenti.uniroma1.it>
2023-03-29 18:19:25 +0200

    updates

---
+++ navigation/dtw.py
@@ -0,0 +1,90 @@
+import sys
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def eucl_dist(x, y):
+    return np.linalg.norm(x-y)
+
+def dtw(t1, t2):
+    n1 = len(t1)
+    n2 = len(t2)
+    C = np.zeros((n1+1, n2+1))
+    C[1:, 0] = float('inf')
+    C[0, 1:] = float('inf')
+
+    for i in np.arange(n1) + 1:
+        for j in np.arange(n2) + 1:
+            C[i,j] = eucl_dist(t1[i - 1], t2[j - 1]) + min(C[i, j - 1], C[i - 1, j - 1], C[i - 1, j])
+
+    dtw = C[n1, n2]
+    return dtw
+
+def plot_trajectories(T1, T2, distance):
+    fig = plt.figure(figsize=(7,7))
+    ax = plt.axes(projection='3d')
+    plt.title(f"Distance between trajectories (DTW): {distance:.3f}", fontsize=10)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    ax.set_zlabel('t')
+    plt.xlim((-20,20))
+    plt.ylim((-20,20))
+    plt.legend(['Human-controlled trajectory', 'CoHAN-planned trajectory'], fontsize=10)
+
+    x1, y1, t1 = [], [], []
+    for elem in T1:
+        x1.append(elem[0])
+        y1.append(elem[1])
+        t1.append(elem[2])
+
+    x2, y2, t2 = [], [], []
+    for elem in T2:
+        x2.append(elem[0])
+        y2.append(elem[1])
+        t2.append(elem[2])
+
+    ax.plot3D(x1,y1,t1,'red')
+    ax.plot3D(x2,y2,t2,'green')
+
+    ax.set_aspect('equal')
+
+    plt.show()
+
+
+fn1 = sys.argv[1]
+fn2 = sys.argv[2]
+
+f1 = open(fn1, "r")
+f2 = open(fn2, "r")
+
+# Trajectories (x,y)
+t1, t2 = [], []
+
+# Trajectories (x,y,t)
+T1, T2 = [], []
+
+lines = f1.readlines()
+for line in lines:
+    if "#" in line:
+        t1_name = line.strip().split(" ")[1]
+        continue
+    t, x, y = line.strip().split(",")
+    t1.append([float(x),float(y)])
+    T1.append([float(x),float(y),float(t)])
+t1 = np.array(t1)
+T1 = np.array(T1)
+
+lines = f2.readlines()
+for line in lines:
+    if "#" in line:
+        t2_name = line.strip().split(" ")[1]
+        continue
+    t, x, y = line.strip().split(",")
+    t2.append([float(x),float(y)])
+    T2.append([float(x),float(y),float(t)])
+t2 = np.array(t2)
+T2 = np.array(T2)
+
+dtw = dtw(t1,t2)
+plot_trajectories(T1,T2,dtw)
+print(dtw)
--- navigation/get_trajectory.py
+++ navigation/get_trajectory.py
@@ -1,4 +1,4 @@
-import rospy, time, sys, os
+import rospy, time, sys
 
 from cohan_msgs.msg import PointArray
 from std_msgs.msg import Header
@@ -12,8 +12,8 @@ class Trajectory(object):
         self.traj_pub = rospy.Publisher("/trajectory", PointArray, queue_size=1)
         self.file = open("trajs/"+filename+".txt", "w")
         self.file.write("# "+filename + "\n")
-        self.curr_x = 0.0
-        self.curr_y = 0.0
+        self.set_start_time = False
+        self.start_time = 0.0
 
     def TrajectoryPub(self):
         rospy.init_node('Trajectory')
@@ -27,12 +27,14 @@ class Trajectory(object):
         self.trajectory.header = msg.header
         self.trajectory.points.append(msg.pose.pose.position)
 
+        if (self.set_start_time == False):
+            self.start_time = time.time()
+            self.set_start_time = True
+
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
-        if (x != self.curr_x or y != self.curr_y):
-            self.file.write(str(x) + "," + str(y) + "\n")
-            self.curr_x = x
-            self.curr_y = y
+        t = time.time() - self.start_time
+        self.file.write(str(t) + "," + str(x) + "," + str(y) + "\n")
 
         if (self.trajectory):
             self.traj_pub.publish(self.trajectory)
