9ca5e0911cf4000e6fc5e9aeaaa414ccedd7267c
metmatera <matera.1795339@studenti.uniroma1.it>
2023-07-02 09:43:56 +0200

    updates

--- navigation/experiments/scenario1/create_target.py
+++ navigation/experiments/scenario1/create_target.py
@@ -1,75 +1,66 @@
 import sys
 import numpy as np
 import math
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
-x0_r = float(sys.argv[1])
-y0_r = float(sys.argv[2])
-v_r = float(sys.argv[3])
+def eucl_dist(x, y):
+    return norm(x-y)
 
-x0_h = float(sys.argv[4])
-y0_h = float(sys.argv[5])
-v_h = float(sys.argv[6])
+filename = sys.argv[1]
+v_h = float(sys.argv[2])
+v_r = 0.6
+d = 3
 
-x1 = v_r / (v_r + v_h) * (x0_h - x0_r - 2.0)
+src_file = open(f'test/{filename}.txt','r')
+lines = src_file.readlines()
+x0_h = float(lines[2].strip().split(',')[3])
+last_raw = lines[-1].strip().split(',')
+xf = float(last_raw[1])
+yf = float(last_raw[2])
 
-xi, xf = 0.0, 12.0
-timesteps = int(xf * 10 + 1)
-total_distance = 10.0 + 2*math.sqrt(2)
+x0, y0 = 0, 0
+x1, y1 = v_r * (x0_h - d) / (v_r + v_h), 0
+x2, y2 = x1 + 1, 1
 
-X = np.linspace(xi, xf, num=timesteps)
+t1 = (x0_h - d) / (v_r + v_h)
+t2 = t1 + math.sqrt(2) / v_r
 
-Y1 = []
-for x in X:
-    if x <= x1 or x > x1 + 4.0:
-        Y1.append(0.0)
-    elif x <= x1 + 1.0:
-        Y1.append(x-x1)
-    elif x <= x1 + 3.0:
-        Y1.append(1.0)
+path_length = x1 + math.sqrt(2) + eucl_dist(np.array([x2,y2]), np.array([xf, yf]))
+total_time = int(path_length * 10 / v_r) / 10
+
+T = np.linspace(0, total_time, num=int(total_time*10 + 1))
+
+X = []
+Y = []
+
+for t in T:
+    if t < t1:
+        X.append(v_r * t)
+        Y.append(0.0)
+    elif t < t2:
+        x = x1 + (math.sqrt(2) * v_r * (t-t1) / 2)
+        X.append(x)
+        Y.append(x - x1)
     else:
-        Y1.append(-x+x1+4)
+        deltax = xf - x2
+        deltay = yf - y2
+        theta = math.atan2(deltay, deltax)
+        x = x2 + (v_r * math.cos(theta) * (t-t2))
+        y = (deltay / deltax) * (x - x2) + y2
+        X.append(x)
+        Y.append(y)
 
-Y1 = np.array(Y1)
-
-Y2 = []
-for x in X:
-    if x <= x1 or x > x1 + 4.0:
-        Y2.append(0.0)
-    elif x <= x1 + 1.0:
-        Y2.append(-x+x1)
-    elif x <= x1 + 3.0:
-        Y2.append(-1.0)
-    else:
-        Y2.append(x-x1-4)
-
-Y2 = np.array(Y2)
-
-f1 = open(f'target/r{v_r}_h{v_h}.txt','w')
-f1.write(f'# Total distance: {total_distance}\n')
-#f2 = open(f'target/r{v_r}_h{v_h}_2.txt','w')
-#f2.write(f'# Total distance: {total_distance}\n')
+target_file = open(f'target/{filename}.txt','w')
+assert len(X) == len(Y)
+t = 0.0
 for i in range(len(X)):
-    f1.write(str(X[i]) + ',' + str(Y1[i]) + '\n')
-    #f2.write(str(X[i]) + ',' + str(Y2[i]) + '\n')
+    target_file.write(f'{t},{X[i]},{Y[i]}\n')
+    t += 0.1
 
-f1.close()
-#f2.close()
+target_file.close()
 
-plt.title(f'Scenario 1 - robot vel: {v_r} - human_vel: {v_h}')
-plt.plot(X,Y2,'r--')
-plt.plot(X,Y1,'g-')
-
-plt.scatter(x0_r, y0_r, s=100, c='black')
-plt.text(x0_r-0.3, y0_r-0.6, 'R_0')
-plt.scatter(x1, 0.0, s=100, c='black')
-plt.text(x1-0.3, -0.6, 'R_1')
-plt.scatter(x0_h, y0_h, s=100, c='black')
-plt.text(x0_h-0.3, y0_h-0.6, 'H_0')
-if (v_h != 0.0):
-    plt.scatter(x1+2.0, 0.0, s=100, c='black')
-    plt.text(x1+1.7, -0.6, 'H_1')
-
+plt.plot(X,Y,'r')
 plt.axis('equal')
 plt.grid()
 plt.show()
---
+++ navigation/experiments/scenario1/test/README.txt
@@ -0,0 +1,5 @@
+Scenario1:
+
+01-04 => human velocity: 0.3 m/s
+05-08 => human velocity: 0.4 m/s
+09-12 => human velocity: 0.5 m/s (human never traked by leg detector)
