d184221e79865f38a0cf122d52276e3da18a0a6d
metmatera <matera.1795339@studenti.uniroma1.it>
2023-07-04 17:46:24 +0200

    updates

--- navigation/experiments/plot.py
+++ navigation/experiments/plot.py
@@ -15,34 +15,25 @@ filename = sys.argv[2]
 file = open(f'scenario{scenario}/test/{filename}.txt', 'r')
 
 times = []
-x_r, y_r, v_r = [], [], []
-x_h, y_h, v_h = [], [], []
-v_rel = []
+x_r, y_r = [], []
+x_h, y_h = [], []
 lines = file.readlines()
 for line in lines:
     if '#' in line:
         continue
-    t, xr, yr, vr, xh, yh, vh = line.strip().split(',')
+    t, xr, yr, xh, yh = line.strip().split(',')
     times.append(float(t))
     x_r.append(float(xr))
     y_r.append(float(yr))
-    v_r.append(float(vr))
     x_h.append(float(xh))
     y_h.append(float(yh))
-    v_h.append(float(vh))
-    if (scenario == '1'):
-        vrel = float(vr) + float(vh)
-    v_rel.append(vrel)
 times = np.array(times)
 x_r = np.array(x_r)
 y_r = np.array(y_r)
-v_r = np.array(v_r)
 x_h = np.array(x_h)
 y_h = np.array(y_h)
-v_h = np.array(v_h)
-v_rel = np.array(v_rel)
 
-fig, ax = plt.subplots(1, 1, figsize=(15,4.5))
+fig, ax = plt.subplots(1, 1, figsize=(15,5))
 
 points_r = np.array([x_r,y_r]).T.reshape(-1,1,2)
 segments_r = np.concatenate([points_r[:-1], points_r[1:]], axis=1)
@@ -86,20 +77,4 @@ ax.plot(x_h[idx], y_h[idx], c='blue', marker='<', markersize=20, label='Human')
 ax.legend()
 
 plt.savefig(f'scenario{scenario}/plots/{filename}_path.png')
-plt.show()
-
-fig, ax = plt.subplots(1, 1, figsize=(8,4.5))
-
-ax.plot(times, v_r, 'red', label='Robot')
-ax.plot(times, v_h, 'blue', label='Human')
-ax.plot(times, v_rel, 'green', label='Relative Velocity')
-v_min = min(v_r.min(), v_h.min(), v_rel.min())
-v_max = max(v_r.max(), v_h.max(), v_rel.max())
-ax.set_xlim(0, times.max())
-ax.set_ylim(v_min-0.1, v_max+0.4)
-ax.legend()
-ax.grid()
-ax.set_xlabel('Time [s]')
-ax.set_ylabel('Speeds [m/s]')
-plt.savefig(f'scenario{scenario}/plots/{filename}_vel.png')
-plt.show()
+#plt.show()
